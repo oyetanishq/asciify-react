563af4b9dd606ef69080bd294207ac328604bf5b
Tanishq Singh <hello@tanishqsingh.com>
2026-05-21 18:58:02 +0530

    update: demo site for v1.0.3

--- demo/src/pages/home.tsx
+++ demo/src/pages/home.tsx
@@ -3,7 +3,22 @@ import { AsciiImage } from "asciify-react";
 function App() {
     return (
         <div style={{ background: "#0e0e0e", width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
-            <AsciiImage fit="contain" width="90%" height="60%" background="#0e0e0e" src="/test.jpg" charset="binary" color noiseScale={0.3} noiseSpeed={1.0} fontSize={9} />
+            <AsciiImage
+                fit="contain"
+                width="90%"
+                height="60%"
+                background="#0e0e0e"
+                src="/test.jpg"
+                charset="binary"
+                color
+                noiseScale={0.3}
+                noiseSpeed={1.0}
+                fontSize={9}
+                mouseMode="attract"
+                hoverStrength={5}
+                hoverAreaSize={200}
+                hoverSpread={20}
+            />
         </div>
     );
 }
