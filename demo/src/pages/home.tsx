6eac21d6dc1fda2d62ff608dd07bb7c87e3852b7
Tanishq Singh <hello@tanishqsingh.com>
2026-05-21 18:16:28 +0530

    refactor: demo update with v1.0.2

--- demo/src/pages/home.tsx
+++ demo/src/pages/home.tsx
@@ -2,19 +2,8 @@ import { AsciiImage } from "asciify-react";
 
 function App() {
     return (
-        <div style={{ background: "#0e0e0e", width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", overflow: "hidden" }}>
-            <AsciiImage
-                responsive
-                background="#0e0e0e"
-                src="/test.jpg"
-                numCols={120}
-                charset="binary"
-                color
-                noiseScale={0.3}
-                noiseSpeed={1.0}
-                fontSize={9}
-                style={{ width: "100%", height: "100%" }}
-            />
+        <div style={{ background: "#0e0e0e", width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <AsciiImage fit="contain" width="90%" height="60%" background="#0e0e0e" src="/test.jpg" charset="binary" color noiseScale={0.3} noiseSpeed={1.0} fontSize={9} />
         </div>
     );
 }
