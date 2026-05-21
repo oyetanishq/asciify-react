6e60ca116d0933614c2f60b137a1dd8d8ff067f7
Tanishq Singh <hello@tanishqsingh.com>
2026-05-21 14:28:25 +0530

    refactor: auto-scale for different screen size

--- demo/src/pages/home.tsx
+++ demo/src/pages/home.tsx
@@ -2,8 +2,19 @@ import { AsciiImage } from "asciify-react";
 
 function App() {
     return (
-        <div className="bg-[#0e0e0e] flex-1 w-full flex justify-center items-center overflow-hidden">
-            <AsciiImage className="scale-50 md:scale-75 lg:scale-100" background="#0e0e0e" src="/test.jpg" numCols={120} charset="binary" color noiseScale={0.3} noiseSpeed={1.0} fontSize={9} />
+        <div style={{ background: "#0e0e0e", width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", overflow: "hidden" }}>
+            <AsciiImage
+                responsive
+                background="#0e0e0e"
+                src="/test.jpg"
+                numCols={120}
+                charset="binary"
+                color
+                noiseScale={0.3}
+                noiseSpeed={1.0}
+                fontSize={9}
+                style={{ width: "100%", height: "100%" }}
+            />
         </div>
     );
 }
